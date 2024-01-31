a2dfb9bd5470caa81b4d5b24e63979a33d9afd19
kwokv244 <vanessakwok908@gmail.com>
2024-01-30 19:51:34 -0500

    1st commit

---
+++ guessher/second/second.js
@@ -0,0 +1,35 @@
+let lists = document.getElementsByClassName("list"); 
+let rightBox = document.getElementById("right")
+let leftBox = document.getElementById("left")
+
+for(list of lists){
+    list.addEventListener("dragstart", function(e){
+        let selected = e.target;
+
+        rightBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+
+        rightBox.addEventListener("drop", function(e){
+            rightBox.appendChild(selected); 
+            selected = null; 
+        });
+
+        leftBox.addEventListener("dragover", function(e){
+            e.preventDefault();
+        });
+
+        leftBox.addEventListener("drop", function(e){
+            leftBox.appendChild(selected); 
+            selected = null; 
+        });
+    })
+}
+
+function correct() {
+    window.location.href = "https://kwokxinyu.github.io/nextlevelcoding/guessher/third/third.html";
+    }
+
+function incorrect() {
+    window.location.href = "https://kwokxinyu.github.io/nextlevelcoding/guessher/wrong/wrong.html"
+}
